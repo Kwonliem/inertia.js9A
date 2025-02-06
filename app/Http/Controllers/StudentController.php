90813ad14bf1a7c65d6b747cf921d42f6265c6a1
Liem <liemyoesuf@gmail.com>
2025-02-06 10:50:39 +0700

    first commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\StudentResource;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $students = StudentResource::collection(Student::all());
+
+        return inertia('Student/Index', [
+            'students' => $students,
+        ]);
+    }
 }
---
+++ resources/js/Pages/Student/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bg-gray-100 py-10">
+        <div class="mx-auto max-w-7x">
+            <div class="px-4 sm:px-6 lg:px-8">
+                <div class="sm:flex sm:items-center">
+                    <div class="sm:flex-auto">
+                        <h1 class="text-xl font-semibold text-gray-900">
+                            Students
+                        </h1>
+                        <p>
+                            List of all students
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
